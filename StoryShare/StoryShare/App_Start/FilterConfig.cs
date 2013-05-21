94e9dc28263ce04f3c845793445e39997373f519
amevyn <anto@amervyn.com>
2013-05-21 23:57:32 +0100

    first

---
+++ StoryShare/StoryShare/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace StoryShare
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
